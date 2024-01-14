d01ffeae27609cffcc695140863501c08abb59aa
azkashah <156576127+azkashah@users.noreply.github.com>
2024-01-14 15:05:36 +0500

    Add files via upload

---
+++ py.c
@@ -0,0 +1,39 @@
+def calculator():
+    print("Simple calculator")
+    print("operation")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.multiply")
+    print("4.Divide")
+
+    try:
+        choice=int(input("Enter operation number(1/2/3/4):"))
+        num1=float(input("Enter first no"))
+        num2=float(input("Enter second no"))
+
+        if choice==1:
+            result = num1+num2
+        elif choice==2:
+            result = num1-num2
+        elif choice==3:
+            result = num1*num2
+        elif choice==4:   
+            result = num1/num2
+        else:
+            print("Error:Divide by zero")
+        else
+        print("invalid operation num")
+    return
+        print("result:(result)")
+          Calculator()    
+          
+
+
+
+              
+              
+:
+            
+
+                         
+        
